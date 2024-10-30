cc97bfb50ee354859aa7eca8c5dcf73fe5cc2c77
Alexandre Silveira de Paula <168464775+alexandresilveiradepaula@users.noreply.github.com>
2024-10-30 14:27:40 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        include './model/Conexao.php';
+        include './model/usuario.php';
+        
+        $u = new usuario();
+        echo $u->validaUsuario('a@a', 'a');
+        print_r($u->recebeUsuario('a@a'));
+        print_r($u->recebeUsuarioPorCampo('nome','a'));
+        
+        echo 'aqui';
+        
+        //var_dump(Conexao::getConexao());
+        
+        
+        
+        
+        ?>
+    </body>
+</html>
---
+++ model/usuario.php
@@ -0,0 +1,93 @@
+<?php
+
+class usuario {
+    
+    public function addUsuario($email,$nome,$senha) {
+        
+        try{
+            $sql = "Insert into usuario Values (?,?,?,?,?,?)";
+            $stmt = Conexao::getConexao()->prepare($sql);
+            $stmt->bindValue(1,$email);
+            $stmt->bindValue(2, md5($email));
+            $stmt->bindValue(3,$nome);
+            $stmt->bindValue(4,$senha);
+            $stmt->bindValue(5,date('Y-m-d'));
+            $stmt->bindValue(6,'padrão.jpg');
+            $stmt->execute();
+            
+            return 'Usuario cadastrado com sucesso';
+            
+        } catch (Exception $ex) {
+            if($ex->errorInfo[1] == 1062)
+            {
+                return 'Usuario já cadastrado';
+            } else{
+                return 'Erro ao cadastrar Usuário';
+            }
+        }
+        
+    }
+    
+    public function validaUsuario($email,$senha) {
+        
+        try{
+            $sql = 'Select * from usuario where  email=? and senha=?';
+            
+            $stmt = Conexao::getConexao()->prepare($sql);
+            $stmt->bindValue(1,$email);
+            $stmt->bindValue(2,$senha);
+            $stmt->execute();
+            
+            $result = $stmt->rowCount();
+            
+            return $result;
+            
+        } catch (Exception $ex) {
+            return false;
+
+        }
+        
+    }
+    
+    public function recebeUsuario($email){
+        try {
+            $sql = "select * from usuario where email='$email'";
+            $stmt = Conexao::getConexao()->prepare($sql);
+            $stmt->bindValue(1,$email);
+            
+            $stmt->execute();
+            
+            if($stmt->rowCount()>0){
+                $result= $stmt->fetch(PDO::FETCH_BOTH);
+                
+                return $result;
+               
+            }
+            
+            return false;
+        } catch (Exception $ex) {
+            return false;
+            
+        }
+    }
+    
+    public function recebeUsuarioPorCampo($campo ,$valor){
+    try {
+        $sql = "Select * from usuario where $campo like '$valor'";
+        
+        $stmt = Conexao::getConexao()->prepare ($sql);
+        
+        $stmt->execute();
+        
+        if ($stmt->rowCount()>0){
+            $result = $stmt->fetchAll(PDO::FETCH_BOTH);
+            
+            return $result;
+        }
+    } catch (Exception $ex) {
+    return false;
+    }
+    }
+ 
+    //put your code here
+}
